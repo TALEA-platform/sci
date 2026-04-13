422b6956170c2e79c88519f92d61ee55753e5ece
Leonardo Bridi <leonardo.bridi-1@studenti.unitn.it>
2026-04-13 11:43:17 +0200

    ghpages

--- webapp/vite.config.js
+++ webapp/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/sci/',
   build: {
     chunkSizeWarningLimit: 900,
     rollupOptions: {
